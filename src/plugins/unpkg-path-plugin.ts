1d8f521545aa72c93d0077de6c9eda7f4e3f0d38
raihan-muhammad <sukasukangoding@gmail.com>
2021-06-30 11:13:06 +0700

    feat: fix nested path problem

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -14,7 +14,7 @@ export const unpkgPathPlugin = () => {
         if (args.path.includes('./') || args.path.includes('../')) {
           return {
             namespace: 'a',
-            path: new URL(args.path, `${args.importer}/`).href,
+            path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`).href,
           };
         }
 
@@ -31,16 +31,17 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              import message from 'medium-test-pkg';
+              import message from 'nested-test-pkg';
               console.log(message);
             `,
           };
         }
 
-        const { data } = await axios.get(args.path);
+        const { data, request } = await axios.get(args.path);
         return {
           loader: 'jsx',
           contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname,
         };
       });
     },
